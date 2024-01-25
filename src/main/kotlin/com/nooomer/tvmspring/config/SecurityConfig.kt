b2ee6e8c67248d7841a98da9f73899376ffaa58d
Nooomer <ampilogov.qaz@gmail.com>
2024-01-25 20:46:26 +0500

    cors

--- src/main/kotlin/com/nooomer/tvmspring/config/SecurityConfig.kt
+++ src/main/kotlin/com/nooomer/tvmspring/config/SecurityConfig.kt
@@ -3,7 +3,10 @@ package com.nooomer.tvmspring.config
 import com.nooomer.tvmspring.db.repositories.UsersRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -18,10 +21,12 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableScheduling
 class SecurityConfig(private val usersRepository: UsersRepository) {
     @Bean
     fun passwordEncoder(): PasswordEncoder? {
@@ -50,13 +55,15 @@ class SecurityConfig(private val usersRepository: UsersRepository) {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedHeaders = listOf("AppType:Mobile", "AppType:Test")
+        configuration.allowedHeaders = listOf("apptype", "Content-type")
+        configuration.allowedOrigins = listOf("http://localhost:3000", "http://localhost:80")
+        configuration.allowedMethods = listOf(CorsConfiguration.ALL)
+        configuration.allowCredentials = true
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
 
         return source
     }
-
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
